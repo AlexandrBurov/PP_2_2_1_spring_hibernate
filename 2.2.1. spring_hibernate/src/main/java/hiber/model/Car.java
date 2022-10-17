3b952eb340c6b9988e444cc637bea5ff45c0491c
macbook <a.d.burov@mail.ru>
2022-10-17 10:36:53 +0400

    .

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -1,14 +1,19 @@
 package hiber.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "car")
 public class Car {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int series;
 
+    @Column(name = "name")
     private String model;
 
-    private int series;
+
+
 
 }
